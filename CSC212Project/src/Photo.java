aedd8c5996e84db6c9aeadd4963b1f3a47386f70
Abdul <Abdul@AbdullahAlsanie>
2025-04-25 16:22:22 +0300

    I add The Photo And BST Classes

--- CSC212Project/src/Photo.java
+++ CSC212Project/src/Photo.java
@@ -1,4 +1,28 @@
 
 public class Photo {
+	
+	String path;
+	LinkedList<String> tags;
+	
+	//Constructor
+	
+	public Photo(String path, LinkedList<String> tags) {
+	// Return the full file name (the path) of the photo. A photo is uniquely identified by its path.
+		
+		this.path = path;
+		this.tags = tags;
+	}
+	
+	public String getPath() {
+	// Return all tags associated with the photo
+		
+		return path;
+	}
+	
+	
+	public LinkedList<String> getTags(){
+	
+		return tags;
+	}
 
 }
