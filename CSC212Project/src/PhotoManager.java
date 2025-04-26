57fed817353537d361cbeaf80ce9524b8c758770
hp <hp@DESKTOP-ENK619A>
2025-04-26 16:32:05 +0300

    added methods(addPhoto,deletePhoto,getPhotos,Photo_Exist).

--- CSC212Project/src/PhotoManager.java
+++ CSC212Project/src/PhotoManager.java
@@ -1,4 +1,55 @@
 
 public class PhotoManager {
+    LinkedList<Photo> allPhotos;
+    public int nbTags = 0;
 
+    public PhotoManager() {
+        allPhotos = new LinkedList<Photo>();
+    }
+
+   
+    public void addPhoto(Photo p) {
+        if (Photo_Exist(allPhotos, p)) return;
+        allPhotos.insert(p);
+    }
+
+    
+    public void deletePhoto(String path) {
+        if (allPhotos.empty()) return;
+
+        allPhotos.findFirst();
+        while (!allPhotos.last()) {
+            if (allPhotos.retrieve().path.equals(path)) {
+                allPhotos.remove();
+                return;
+            }
+            allPhotos.findNext();
+        }
+
+        if (allPhotos.retrieve().path.equals(path)) {
+            allPhotos.remove();
+        }
+    }
+
+   
+    public LinkedList<Photo> getPhotos() {
+        return allPhotos;
+    }
+
+   
+    public boolean Photo_Exist(LinkedList<Photo> L, Photo p) {
+        if (L.empty()) return false;
+
+        L.findFirst();
+        while (!L.last()) {
+            if (L.retrieve().path.equals(p.path))
+                return true;
+            L.findNext();
+        }
+
+        if (L.retrieve().path.equals(p.path))
+            return true;
+
+        return false;
+    }
 }
